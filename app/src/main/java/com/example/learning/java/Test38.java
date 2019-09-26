76a1802b273d1900a9003b3b09557bf61776c5b1
Zhang Chaoxing <zhangcx@nemo-inc.com>
2019-09-26 17:40:50 +0800

    增加 WeakReference 的测试

--- app/src/main/java/com/example/learning/java/Test38.java
+++ app/src/main/java/com/example/learning/java/Test38.java
@@ -1,7 +1,20 @@
 package com.example.learning.java;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+
 public class Test38 {
     public static void main(String[] args) {
+        Object o = new Object();
 
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        Reference<Object> reference = new WeakReference<>(o, referenceQueue);
+
+        o = null;
+        System.gc();
+
+        Utils.println(reference.get());
+        Utils.println(reference.isEnqueued());
     }
 }
